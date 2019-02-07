4ac62e6d7a5d72b4e9f113e2336d6b3103eb3f8c
Marc <zunly@hotmail.com>
2019-02-07 18:56:30 +0800

    token拿到了

--- token-service/src/main/java/com/wulala/tokenservice/TokenServiceApplication.java
+++ token-service/src/main/java/com/wulala/tokenservice/TokenServiceApplication.java
@@ -2,9 +2,19 @@ package com.wulala.tokenservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@EnableEurekaClient
 public class TokenServiceApplication {
+
+    @RequestMapping("/")
+    public String getTokenTest() {
+        return "token from token service";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TokenServiceApplication.class, args);
